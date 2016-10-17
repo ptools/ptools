ed5d1c867bbe460402fe1a07d192d56efc853a61
Benoist LAURENT <benoist.laurent@gmail.com>
2016-10-17 15:46:15 +0200

    spatial: implemented SpatialObject.translate; better check coord3d initialization

--- pyptools/spatial.py
+++ pyptools/spatial.py
@@ -34,10 +34,21 @@ def coord3d(value=(0, 0, 0)):
     if isinstance(value, (int, float)):
         return numpy.full((3,), value, dtype=float)
     value = numpy.array(value, dtype=float)
-    if value.shape != (3, ):
-        err = '3-d coordinates should be a scalar or '\
-              '1 x 3 shaped-array ({} shaped-array found)'
-        err = err.format(value.shape)
+
+    if len(value.shape) == 1:
+        if value.shape[0] != 3:
+            err = '3-d coordinates should be a scalar or '\
+                  '1 x 3 shaped-array ({} shaped-array found)'
+            err = err.format(value.shape)
+            raise ValueError(err)
+    elif len(value.shape) == 2:
+        if value.shape[1] != 3:
+            err = '3-d coordinate array should be N x 3 '\
+                  '({} found)'.format(value.shape)
+            raise ValueError(err)
+    else:
+        err = '3-d coordinate array should have at most 2 dimensions '\
+              '({} found)'.format(len(value.shape))
         raise ValueError(err)
     return value
 
--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -10,35 +10,91 @@ from pyptools.spatial import SpatialObject, coord3d
 from . import assert_array_almost_equal
 
 
-class TestSpatialObject(unittest.TestCase):
+class TestSpatialObjectVector(unittest.TestCase):
+    """Test SpatialObject methods on a vector."""
 
     def test_empty_constructor(self):
         o = SpatialObject()
         assert_array_almost_equal(o.coords, (0, 0, 0))
 
-    # def test_translate_vector(self):
-    #     o = SpatialObject()
+    def test_constructor(self):
+        o = SpatialObject((1, 1, 1))
+        assert_array_almost_equal(o.coords, (1, 1, 1))
+
+    def test_translate_scalar(self):
+        o = SpatialObject((0, 0, 0))
+        o.translate(1)
+        assert_array_almost_equal(o.coords, (1, 1, 1))
+
+    def test_translate_vector(self):
+        o = SpatialObject((0, 0, 0))
+        o.translate((1, 2, 3))
+        assert_array_almost_equal(o.coords, (1, 2, 3))
+
+
+class TestSpatialObjectArray(unittest.TestCase):
+    """Test SpatialObject methods on an array of vector."""
+
+    def setUp(self):
+        import numpy
+        array = numpy.array(( (0, 0, 0), (1, 1, 1)), dtype=float)
+        self.obj = SpatialObject(array)
+
+    def test_constructor(self):
+        self.assertEqual(self.obj.coords.shape, (2, 3))
+        # assert_array_almost_equal(o.coords, (1, 1, 1))
+
+
 
 
 
 def test_coord3d_no_args():
     c = coord3d()
     assert c.shape == (3, )
-    assert c[0] == c[1] == c[2] == 0
+    assert_array_almost_equal(c, (0, 0, 0))
 
 
-def test_coords3d_scalar():
+def test_coord3d_scalar():
     c = coord3d(1)
     assert c.shape == (3, )
-    assert c[0] == c[1] == c[2] == 1
+    assert_array_almost_equal(c, (1, 1, 1))
 
-def test_coords3d_array():
+
+def test_coord3d_array():
     c = coord3d([2, 2, 2])
     assert c.shape == (3, )
-    assert c[0] == c[1] == c[2] == 2
+    assert_array_almost_equal(c, (2, 2, 2))
 
-def test_coords3d_fails():
+
+def test_coord3d_fails():
     with pytest.raises(ValueError) as excinfo:
         c = coord3d([2, 2])
     err = '3-d coordinates should be a scalar or 1 x 3 shaped-array'
     assert err in str(excinfo.value)
+
+
+def test_coord3d_array():
+    array = ((0, 0, 0), (1, 1, 1))
+    c = coord3d(array)
+    assert c.shape == (2, 3)
+    assert_array_almost_equal(c[0], (0, 0, 0))
+    assert_array_almost_equal(c[1], (1, 1, 1))
+
+def test_coord3d_array_fails():
+    array = ((0, 0, 0, 0), (1, 1, 1, 1))
+    with pytest.raises(ValueError) as excinfo:
+        coord3d(array)
+    err = '3-d coordinate array should be N x 3'
+    assert err in str(excinfo.value)
+
+def test_coord3d_array_fails2():
+    """Test raises the appropriate exeception when array has more that
+    2 dimensions."""
+    array = (((0, 0, 0, 0), (1, 1, 1, 1)),
+             ((0, 0, 0, 0), (1, 1, 1, 1)),
+             ((0, 0, 0, 0), (1, 1, 1, 1)))
+    with pytest.raises(ValueError) as excinfo:
+        coord3d(array)
+    err = '3-d coordinate array should have at most 2 dimensions'
+    assert err in str(excinfo.value)
+
