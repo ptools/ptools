63a69b80d4d7e26e55d56bcee2087d36a8ff160e
Benoist LAURENT <benoist.laurent@gmail.com>
2016-10-26 08:16:53 +0200

    spatial: implemented some rotation routines - not working yet

--- pyptools/spatial.py
+++ pyptools/spatial.py
@@ -3,6 +3,9 @@
 (mostly 3D) data."""
 
 
+import math
+from math import cos, sin
+
 import numpy
 
 
@@ -61,7 +64,54 @@ def translate(coords, v):
     """In-place translation of coordinates by a vector.
 
     Args:
-        coords (numpy.array): N x 3 shaped array
+        coords (numpy.ndarray): N x 3 shaped array
         v (iterable[float, int]): 1 x 3 shaped vector
     """
     numpy.add(coords, v, coords)
+
+
+def rotation(alpha=0.0, beta=0.0, gamma=0.0):
+    """Return the rotation matrix around the X, Y and Z axes.
+
+    The matrix rotates first along X-axis, then Y, then Z.
+
+    Args:
+        alpha (float): rotation angle around the X-axis (in degrees)
+        beta (float): rotation angle around the Y-axis (in degrees)
+        gamma (float): rotation angle around the Z-axis (in degrees)
+
+    Returns:
+        numpy.ndarray: 4x4 rotation matrix.
+    """
+    alpha = math.radians(alpha)
+    beta = math.radians(beta)
+    gamma = math.radians(gamma)
+    r = numpy.identity(4)
+    r[0, 0] = cos(beta) * cos(gamma)
+    r[0, 1] = -cos(beta) * sin(gamma)
+    r[0, 2] = sin(beta)
+
+    r[1, 0] = cos(alpha) * sin(gamma) + sin(alpha) * sin(beta) * cos(gamma)
+    r[1, 1] = cos(alpha) * cos(gamma) - sin(alpha) * sin(beta) * sin(gamma)
+    r[1, 2] = -sin(alpha) * cos(beta)
+
+    r[2, 0] = sin(alpha) * sin(gamma) - cos(alpha) * sin(beta) * cos(gamma)
+    r[2, 1] = sin(alpha) * cos(gamma) + cos(alpha) * sin(beta) * sin(gamma)
+    r[2, 2] = cos(alpha) * cos(beta)
+    return r
+
+
+def rotate(coords, alpha=0.0, beta=0.0, gamma=0.0):
+    """In-place rotation of coordinates around X, Y and Z axes.
+
+    Args:
+        coords (numpy.ndarray): N x 3 shaped array
+        alpha (float): rotation angle around the X-axis
+        beta (float): rotation angle around the Y-axis
+        gamma (float): rotation angle around the Z-axis
+    """
+    matrix = rotation(alpha, beta, gamma)
+    return numpy.dot(coords, matrix[:3, :3])
+
+
+
