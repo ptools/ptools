ddbaa8f9b56a87bd7a1922181e228dd50e168176
Benoist LAURENT <benoist.laurent@gmail.com>
2016-10-27 13:42:45 +0200

    unit tests for transform

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -46,33 +46,6 @@ class TestSpatialObjectArray(unittest.TestCase):
         self.assertEqual(self.obj.coords.shape, (2, 3))
         assert_array_almost_equal(self.obj.coords[1], (1, 1, 1))
 
-    def test_rotate(self):
-        # Coordinates are [(1, 11, 21), (2, 12, 22), ..., (10, 20, 30)]
-        coords = numpy.array(list([i + 1., i + 11., i + 21.] for i in range(10)))
-
-        # Initialize SpatialObject.
-        o = spatial.SpatialObject(coords)
-
-        # Rotate by 12° along X-axis.
-        o.rotate(alpha=12)
-
-        # Reference coordinates calculated with VMD::
-        #     set m [transaxis x 12]
-        #     set sel [atomselect 0 "all"]
-        #     $sel move $m
-        ref_coords = [[ 1.0000000,    6.393478,   22.828129],
-                      [ 2.0000000,    7.163715,   24.014187],
-                      [ 3.0000000,    7.933951,   25.200247],
-                      [ 4.0000000,    8.704186,   26.386307],
-                      [ 5.0000000,    9.474422,   27.572367],
-                      [ 6.0000000,   10.244658,   28.758427],
-                      [ 7.0000000,   11.014894,   29.944485],
-                      [ 8.0000000,   11.785130,   31.130545],
-                      [ 9.0000000,   12.555366,   32.316605],
-                      [10.0000000,   13.325602,   33.502663]]
-
-        assert_array_almost_equal(o.coords, ref_coords, decimal=5)
-
 
 class TestRotation(unittest.TestCase):
 
@@ -151,6 +124,13 @@ class TestRotation(unittest.TestCase):
         m = spatial.rotation(gamma=10)
         assert_array_almost_equal(m, ref)
 
+    def test_rotation_XYZ(self):
+        """Test rotation in X, Y and Z is equivalent to successive rotation
+        in X, then Y, then Z."""
+        m = spatial.rotation(alpha=10, beta=10, gamma=10)
+        ref = spatial.rotation(alpha=10).dot(spatial.rotation(beta=10)).dot(spatial.rotation(gamma=10))
+        assert_array_almost_equal(m, ref)
+
     def test_rotate(self):
         # Coordinates are [(1, 11, 21), (2, 12, 22), ..., (10, 20, 30)]
         coords = numpy.array(list([i + 1., i + 11., i + 21.] for i in range(10)))
@@ -174,6 +154,126 @@ class TestRotation(unittest.TestCase):
                       [10.0000000,   13.325602,   33.502663]]
 
         assert_array_almost_equal(coords, ref_coords, decimal=5)
+
+    def test_rotate_so(self):
+        # Coordinates are [(1, 11, 21), (2, 12, 22), ..., (10, 20, 30)]
+        coords = numpy.array(list([i + 1., i + 11., i + 21.] for i in range(10)))
+
+        # Initialize SpatialObject.
+        o = spatial.SpatialObject(coords)
+
+        # Rotate by 12° along X-axis.
+        o.rotate(alpha=12)
+
+        # Reference coordinates calculated with VMD::
+        #     set m [transaxis x 12]
+        #     set sel [atomselect 0 "all"]
+        #     $sel move $m
+        ref_coords = [[ 1.0000000,    6.393478,   22.828129],
+                      [ 2.0000000,    7.163715,   24.014187],
+                      [ 3.0000000,    7.933951,   25.200247],
+                      [ 4.0000000,    8.704186,   26.386307],
+                      [ 5.0000000,    9.474422,   27.572367],
+                      [ 6.0000000,   10.244658,   28.758427],
+                      [ 7.0000000,   11.014894,   29.944485],
+                      [ 8.0000000,   11.785130,   31.130545],
+                      [ 9.0000000,   12.555366,   32.316605],
+                      [10.0000000,   13.325602,   33.502663]]
+
+        assert_array_almost_equal(o.coords, ref_coords, decimal=5)
+
+
+
+class TestTransformation(unittest.TestCase):
+    """Test transformations thanks to 4 x 4 matrices."""
+
+
+    def test_transform_rotation(self):
+        """Test transformation with a rotation matrix.
+
+        In this test, the last row and columns are identity.
+        """
+        # Matrix calculated with VMD that correspond to a rotation
+        # in the X-axis by 10°.
+        m = numpy.array([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.984807753012208, -0.17364817766693033, 0.0],
+            [0.0, 0.17364817766693033, 0.984807753012208, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ])
+
+        # Coordinates are [(1, 11, 21), (2, 12, 22), ..., (10, 20, 30)]
+        coords = numpy.array(list([i + 1., i + 11., i + 21.] for i in range(10)))
+
+        ref = coords.copy()
+        spatial.rotate(ref, alpha=10)
+
+        spatial.transform(coords, m)
+        assert_array_almost_equal(coords, ref)
+
+    def test_transform(self):
+        """Test transformation with a random matrix."""
+        # Input transformation matrix (just a random matrix).
+        m = numpy.array([[ 0.72898284,  0.43879558,  0.31011219,  0.32696626],
+                         [ 0.74291592,  0.00244003,  0.20881428,  0.91014385],
+                         [ 0.71045798,  0.85828462,  0.71570462,  0.96008097],
+                         [ 0.4331563 ,  0.30949433,  0.9348666 ,  0.48177513]])
+
+
+        # Coordinates are [(1, 11, 21), (2, 12, 22), ..., (10, 20, 30)]
+        coords = numpy.array(list([i + 1., i + 11., i + 21.] for i in range(10)))
+
+        # Reference calculated with VMD
+        # with input topology = tests/data/test_10atoms.pdb.
+        ref = numpy.array([
+            [0.5175050497055054, 0.253219336271286, 1.0914303064346313],
+            [0.5412969589233398, 0.2738751769065857, 1.1091270446777344],
+            [0.5621657967567444, 0.291993111371994, 1.1246496438980103],
+            [0.5806189775466919, 0.3080138564109802, 1.1383754014968872],
+            [0.5970528721809387, 0.3222815990447998, 1.1505991220474243],
+            [0.611781895160675, 0.33506909012794495, 1.1615549325942993],
+            [0.6250582337379456, 0.3465954065322876, 1.171429991722107],
+            [0.6370866894721985, 0.35703831911087036, 1.1803770065307617],
+            [0.6480352282524109, 0.3665436804294586, 1.1885206699371338],
+            [0.6580431461334229, 0.37523239850997925, 1.1959648132324219]
+        ])
+
+        spatial.transform(coords, m)
+        assert_array_almost_equal(coords, ref)
+
+    def test_transform_so(self):
+        """Test spatial.SpatialObject.transform."""
+        # Input transformation matrix (just a random matrix).
+        m = numpy.array([[ 0.72898284,  0.43879558,  0.31011219,  0.32696626],
+                         [ 0.74291592,  0.00244003,  0.20881428,  0.91014385],
+                         [ 0.71045798,  0.85828462,  0.71570462,  0.96008097],
+                         [ 0.4331563 ,  0.30949433,  0.9348666 ,  0.48177513]])
+
+        # Coordinates are [(1, 11, 21), (2, 12, 22), ..., (10, 20, 30)]
+        coords = numpy.array(list([i + 1., i + 11., i + 21.] for i in range(10)))
+
+        # Initialize SpatialObject.
+        o = spatial.SpatialObject(coords)
+
+        # Rotate by 12° along X-axis.
+        o.transform(m)
+
+        # Reference calculated with VMD
+        # with input topology = tests/data/test_10atoms.pdb.
+        ref = numpy.array([
+            [0.5175050497055054, 0.253219336271286, 1.0914303064346313],
+            [0.5412969589233398, 0.2738751769065857, 1.1091270446777344],
+            [0.5621657967567444, 0.291993111371994, 1.1246496438980103],
+            [0.5806189775466919, 0.3080138564109802, 1.1383754014968872],
+            [0.5970528721809387, 0.3222815990447998, 1.1505991220474243],
+            [0.611781895160675, 0.33506909012794495, 1.1615549325942993],
+            [0.6250582337379456, 0.3465954065322876, 1.171429991722107],
+            [0.6370866894721985, 0.35703831911087036, 1.1803770065307617],
+            [0.6480352282524109, 0.3665436804294586, 1.1885206699371338],
+            [0.6580431461334229, 0.37523239850997925, 1.1959648132324219]
+        ])
+
+        assert_array_almost_equal(o.coords, ref)
 
 
 def test_coord3d_no_args():
