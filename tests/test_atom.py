8f972d1dbf584147745d33d754e42327f921a7b9
Benoist LAURENT <benoist.laurent@gmail.com>
2020-03-12 16:52:15 +0100

    add BaseAtom.topdb() unit test

--- tests/test_atom.py
+++ tests/test_atom.py
@@ -14,33 +14,33 @@ class TestBaseAtom(unittest.TestCase):
 
     def test_empty_initializer(self):
         atom = BaseAtom()
-        self.assertEqual(atom.name, 'XXX')
-        self.assertEqual(atom.resname, 'XXX')
-        self.assertEqual(atom.chain, 'X')
+        self.assertEqual(atom.name, "XXX")
+        self.assertEqual(atom.resname, "XXX")
+        self.assertEqual(atom.chain, "X")
         self.assertEqual(atom.index, 0)
         self.assertEqual(atom.resid, 0)
         self.assertEqual(atom.charge, 0.0)
         assert_array_almost_equal(atom.coords, (0, 0, 0))
 
     def test_initialization(self):
-        atom = BaseAtom(name='CA', resname='ALA', chain='A',
+        atom = BaseAtom(name="CA", resname="ALA", chain="A",
                         index=42, resid=17, charge=2.0, coords=(1, 2, 3))
-        self.assertEqual(atom.name, 'CA')
-        self.assertEqual(atom.resname, 'ALA')
-        self.assertEqual(atom.chain, 'A')
+        self.assertEqual(atom.name, "CA")
+        self.assertEqual(atom.resname, "ALA")
+        self.assertEqual(atom.chain, "A")
         self.assertEqual(atom.index, 42)
         self.assertEqual(atom.resid, 17)
         self.assertEqual(atom.charge, 2.0)
         assert_array_almost_equal(atom.coords, (1, 2, 3))
 
     def test_initialize_with_bad_coordinates(self):
-        err = '3-d coordinates should be a scalar or 1 x 3 shaped-array'
+        err = "3-d coordinates should be a scalar or 1 x 3 shaped-array"
         with self.assertRaisesRegex(ValueError, err):
             BaseAtom(coords=(1, 2))
 
     def test_set_bad_coordinates(self):
         atom = BaseAtom()
-        err = '3-d coordinates should be a scalar or 1 x 3 shaped-array'
+        err = "3-d coordinates should be a scalar or 1 x 3 shaped-array"
         with self.assertRaisesRegex(ValueError, err):
             atom.coords = (1, 2)
 
@@ -54,12 +54,12 @@ class TestBaseAtom(unittest.TestCase):
         # Check that all arguments are adequatly set from original atom
         # and that atom coordinates are not a reference to
         # the initial atom coordinates.
-        atom = BaseAtom(name='CA', resname='ALA', chain='A',
+        atom = BaseAtom(name="CA", resname="ALA", chain="A",
                         index=42, resid=17, charge=2.0, coords=(1, 2, 3))
         atom_copy = atom.copy()
-        self.assertEqual(atom_copy.name, 'CA')
-        self.assertEqual(atom_copy.resname, 'ALA')
-        self.assertEqual(atom_copy.chain, 'A')
+        self.assertEqual(atom_copy.name, "CA")
+        self.assertEqual(atom_copy.resname, "ALA")
+        self.assertEqual(atom_copy.chain, "A")
         self.assertEqual(atom_copy.index, 42)
         self.assertEqual(atom_copy.resid, 17)
         self.assertEqual(atom_copy.charge, 2.0)
@@ -71,18 +71,24 @@ class TestBaseAtom(unittest.TestCase):
         # Check that when using copy constructor, all arguments are
         # adequatly set and that atom coordinates are not a reference to
         # the initial atom coordinates.
-        parent = BaseAtom(name='CA', resname='ALA', chain='A',
+        parent = BaseAtom(name="CA", resname="ALA", chain="A",
                           index=42, resid=17, charge=2.0, coords=(1, 2, 3))
         atom = BaseAtom(orig=parent)
-        self.assertEqual(atom.name, 'CA')
-        self.assertEqual(atom.resname, 'ALA')
-        self.assertEqual(atom.chain, 'A')
+        self.assertEqual(atom.name, "CA")
+        self.assertEqual(atom.resname, "ALA")
+        self.assertEqual(atom.chain, "A")
         self.assertEqual(atom.index, 42)
         self.assertEqual(atom.resid, 17)
         self.assertEqual(atom.charge, 2.0)
         assert_array_almost_equal(atom.coords, (1, 2, 3))
         parent.coords = (0, 0, 0)
         assert_array_almost_equal(atom.coords, (1, 2, 3))
+
+    def test_topdb(self):
+        atom = BaseAtom(name="CA", resname="ALA", chain="A",
+                        index=42, resid=17, charge=2.0, coords=(1, 2, 3))
+        reference_string = "ATOM     42  CA  ALA A  17       1.000   2.000   3.000  1.00  0.00           C"
+        self.assertEqual(atom.topdb(), reference_string)
 
 
 class TestAtom(unittest.TestCase):
@@ -91,13 +97,13 @@ class TestAtom(unittest.TestCase):
         # Check that when using copy constructor, all arguments are
         # adequatly set and that atom coordinates are not a reference to
         # the initial atom coordinates.
-        orig = BaseAtom(name='CA', resname='ALA', chain='A',
+        orig = BaseAtom(name="CA", resname="ALA", chain="A",
                         index=42, resid=17, charge=2.0, coords=(1, 2, 3))
         collection = AtomCollection([orig])
         atom = collection.atoms[0]
-        self.assertEqual(atom.name, 'CA')
-        self.assertEqual(atom.resname, 'ALA')
-        self.assertEqual(atom.chain, 'A')
+        self.assertEqual(atom.name, "CA")
+        self.assertEqual(atom.resname, "ALA")
+        self.assertEqual(atom.chain, "A")
         self.assertEqual(atom.index, 42)
         self.assertEqual(atom.resid, 17)
         self.assertEqual(atom.charge, 2.0)
