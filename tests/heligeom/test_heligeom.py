b25f05507ee65788574f9f852eb2f9530d235c84
Benoist LAURENT <benoist.laurent@gmail.com>
2020-08-24 13:19:08 +0200

    test_heligeom: refactoring

--- tests/heligeom/test_heligeom.py
+++ tests/heligeom/test_heligeom.py
@@ -15,67 +15,54 @@ TEST_1A74_PROT_RED = os.path.join(os.path.dirname(__file__), 'data', '1A74_prot.
 
 
 class TestHeligeom(unittest.TestCase):
+    def setUp(self):
+        self.mono1 = RigidBody(TEST_1A74_PROT_RED)
+        self.dx = 15
+        self.mono2 = move_rigidbody(self.mono1, x=self.dx)
+
+
     def test_analyze_x_translate(self):
-        mono1 = RigidBody(TEST_1A74_PROT_RED)
-        mono2 = RigidBody(TEST_1A74_PROT_RED)
-        delta_x = 15
-        tr = coord3d(delta_x, 0, 0)
-        mono2.translate(tr)
-        hp = heli_analyze(mono1, mono2)
+        hp = heli_analyze(self.mono1, self.mono2)
         self.assertAlmostEqual(hp.angle, 0.0)
-        self.assertAlmostEqual(hp.normtranslation, delta_x)
+        self.assertAlmostEqual(hp.normtranslation, self.dx)
         self.assertAlmostEqual(hp.unit[0], 1.0)
         self.assertAlmostEqual(hp.unit[1], 0.0)
         self.assertAlmostEqual(hp.unit[2], 0.0)
 
     def test_analyze_x_translate_rotate(self):
-        mono1 = RigidBody(TEST_1A74_PROT_RED)
-        mono2 = RigidBody(TEST_1A74_PROT_RED)
-        delta_x = 15
-        tr = coord3d(delta_x, 0, 0)
-        mono2.translate(tr)
-
         point = coord3d(0, 0, 0)
         axis = coord3d(1, 0, 0)
         angle = math.pi / 4
-        mono2.ab_rotate(point, axis, angle)
+        self.mono2.ab_rotate(point, axis, angle)
 
-        hp = heli_analyze(mono1, mono2)
+        hp = heli_analyze(self.mono1, self.mono2)
         self.assertAlmostEqual(hp.angle, angle)
-        self.assertAlmostEqual(hp.normtranslation, delta_x)
+        self.assertAlmostEqual(hp.normtranslation, self.dx)
         self.assertAlmostEqual(hp.unit[0], 1.0)
         self.assertAlmostEqual(hp.unit[1], 0.0)
         self.assertAlmostEqual(hp.unit[2], 0.0)
 
     def test_heli_construct(self):
         """Tests that heligeom.heli_construct does the same thing as heligeom.extend."""
-        mono1 = RigidBody(TEST_1A74_PROT_RED)
-        mono2 = RigidBody(TEST_1A74_PROT_RED)
-        delta_x = 15
-        tr = coord3d(delta_x, 0, 0)
-        mono2.translate(tr)
+        hp = heli_analyze(self.mono1, self.mono2)
 
-        hp = heli_analyze(mono1, mono2)
-
-        target = extend(hp, mono1, N=15)
-        result = heli_construct(mono1, hp, N=15)
+        target = extend(hp, self.mono1, N=15)  # N is random
+        result = heli_construct(self.mono1, hp, N=15)
         assert_array_equal(result.coords, target.coords)
 
     def test_Z_true_not_implemented(self):
         """Tests that using heligeom.extend with Z=true raises an error."""
-        mono1 = RigidBody(TEST_1A74_PROT_RED)
-        mono2 = RigidBody(TEST_1A74_PROT_RED)
-        delta_x = 15
-        tr = coord3d(delta_x, 0, 0)
-        mono2.translate(tr)
-        hp = heli_analyze(mono1, mono2)
-        target = extend(hp, mono1, N=15)
+        hp = heli_analyze(self.mono1, self.mono2)  # N is random
+        target = extend(hp, self.mono1, N=15)
 
         with self.assertRaises(NotImplementedError):
-            result = heli_construct(mono1, hp, N=15, Z=True)
+            result = heli_construct(self.mono1, hp, N=15, Z=True)
 
 
-
+def move_rigidbody(rb, x=0, y=0, z=0):
+    out = rb.copy()
+    out.moveby([x, y, z])
+    return out
 
 
 if __name__ == "__main__":
