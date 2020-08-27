7283c74402662d58a80a69f2352c2e0eaf567f15
Benoist LAURENT <benoist.laurent@gmail.com>
2020-08-27 11:04:03 +0200

    ptools.__init__: fix submodule declaration

--- ptools/__init__.py
+++ ptools/__init__.py
@@ -4,4 +4,18 @@ __author__ = """Benoist LAURENT"""
 __email__ = 'benoistlaurent@gmail.com'
 __version__ = '0.1.0'
 
+
+
+from . import (
+    atom,
+    forcefield,
+    heligeom,
+    io,
+    pairlist,
+    rigidbody,
+    spatial,
+    superpose,
+    tables
+)
+
 from .rigidbody import RigidBody
