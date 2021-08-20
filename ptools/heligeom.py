e1b714546f812ea3d2e8e760e204031f155aeed2
Benoist LAURENT <benoist.laurent@gmail.com>
2021-08-20 16:18:45 +0200

    heligeom: type hinting

--- ptools/heligeom.py
+++ ptools/heligeom.py
@@ -6,23 +6,23 @@ Some more documentation coming soon.
 import string
 import numpy as np
 
-import ptools.rigidbody
-from ptools.superpose import mat_trans_2_screw, fit_matrix
+from ptools.rigidbody import RigidBody
+from ptools.superpose import Screw, mat_trans_2_screw, fit_matrix
 
 
-def heli_analyze(mono1, mono2):
+def heli_analyze(mono1: RigidBody, mono2: RigidBody) -> np.ndarray:
     """Returns the screw transformation from mono1 to mono2."""
     return mat_trans_2_screw(fit_matrix(mono1, mono2))
 
 
-def heli_construct(mono1, hp, N, Z=False):
+def heli_construct(mono1: RigidBody, hp: Screw, N: int, Z: bool = False) -> RigidBody:
     """Constructs a N-mer by repeating the screw transormation hp."""
     return extend(hp, mono1, N, Z)
 
 
-def extend(hp, mono1, N, Z=False):
-    final = ptools.RigidBody()
-    monoTest = mono1.copy()
+def extend(hp: Screw, mono1: RigidBody, N: int, Z: bool = False):
+    final = RigidBody()
+    mono_test = mono1.copy()
     chain_id = 0
     origin = hp.point
     axis = hp.unit
@@ -31,18 +31,18 @@ def extend(hp, mono1, N, Z=False):
         origin = np.zeros(3)
         axis = np.array([0.0, 0, 1])
 
-        # Align the screw axis on Z-axis and apply the transformation on monoTest
-        monoTest.orient(hp.unit, [0.0, 0.0, 1.0])
+        # Align the screw axis on Z-axis and apply the transformation on mono_test
+        mono_test.orient(hp.unit, [0.0, 0.0, 1.0])
 
-    monoTest.set_chain(string.ascii_uppercase[chain_id % 26])
-    final += monoTest
+    mono_test.set_chain(string.ascii_uppercase[chain_id % 26])
+    final += mono_test
     chain_id += 1
 
     for _ in range(N - 1):
-        monoTest.ab_rotate(origin, origin + axis, hp.angle)
-        monoTest.translate(axis * hp.normtranslation)
-        monoTest.set_chain(string.ascii_uppercase[chain_id % 26])
-        final += monoTest
+        mono_test.ab_rotate(origin, origin + axis, hp.angle)
+        mono_test.translate(axis * hp.normtranslation)
+        mono_test.set_chain(string.ascii_uppercase[chain_id % 26])
+        final += mono_test
         chain_id += 1
 
     return final
