7b5389fe9f94e91cdcd92a6a5e8487fb8fb67eee
benoistlaurent <benoist.laurent@gmail.com>
2021-10-14 11:06:13 +0200

    refactoring: testing: create the dummy module that handles dummy object creation

--- tests/testing/moreassert.py
+++ tests/testing/moreassert.py
@@ -5,6 +5,8 @@ import numpy
 
 from ptools.atom import BaseAtom
 
+from .dummy import DUMMY_ATOM_ATTRS
+
 
 assert_array_almost_equal = numpy.testing.assert_array_almost_equal
 assert_array_equal = numpy.testing.assert_array_equal
@@ -28,24 +30,9 @@ def assert_array_not_almost_equal(array1, array2):
     raise AssertionError("arrays are almost equal")
 
 
-
-def dummy_atom():
-    """Creates a dummy atoms used in several tests."""
-    return BaseAtom(
-        name="CA",
-        resname="ALA",
-        chain="A",
-        index=42,
-        resid=17,
-        charge=2.0,
-        coords=(1, 2, 3),
-    )
-
 def assert_dummy_atom_initialization_ok(self: unittest.TestCase, dummy: BaseAtom):
-    self.assertEqual(dummy.name, "CA")
-    self.assertEqual(dummy.resname, "ALA")
-    self.assertEqual(dummy.chain, "A")
-    self.assertEqual(dummy.index, 42)
-    self.assertEqual(dummy.resid, 17)
-    self.assertEqual(dummy.charge, 2.0)
-    assert_array_almost_equal(dummy.coords, (1, 2, 3))
+    """Asserts dummy atom initialization went fine."""
+    for attr, value in DUMMY_ATOM_ATTRS.items():
+        if attr != "coords":
+            self.assertEqual(getattr(dummy, attr), value)
+    assert_array_almost_equal(dummy.coords, DUMMY_ATOM_ATTRS["coords"])
