ptools/ptools

commit 40d2eb0617a5301bd46ebe912ff559c4ebfeac60

fixes typing and linting

ptools/io/pdb.py
@@ -1,23 +1,10 @@
 """Protein Data Bank format I/O."""
 
-import abc
-from typing import Protocol, Sequence, Tuple, Union
+from typing import Sequence, Tuple, Union
 
 from ..atomattrs import AtomAttrs
 from ..atomcollection import AtomCollection
 from .._typing import FilePath
-
-
-class FromPDB(Protocol):
-    """Abstract Base class for classes which can be initialized from PDB files.
-
-    Ensures that child classes implement the `init_from_pdb` method.
-    """
-
-    @classmethod
-    @abc.abstractmethod
-    def from_pdb(cls, path: FilePath):
-        """Initializes internal components from data read in PDB file."""
 
 
 class InvalidPDBFormatError(IOError):
@@ -29,7 +16,7 @@ class InvalidPDBAtomLineError(ValueError):
 
     def __init__(self, header, *args):
         message = f'Invalid atom line: expected "ATOM" or "HETATM" as header, found "{header}"'
-        super(InvalidPDBAtomLineError, self).__init__(message, *args)
+        super().__init__(message, *args)
 
 
 class PDBLine(str):
@@ -40,7 +27,7 @@ class PDBLine(str):
     """
 
     def __init__(self, *args) -> None:
-        super().__init__()
+        super().__init__(*args)
         self.header = self[0:6].strip()
 
     def is_atom(self) -> bool:

ptools/io/formatters/pdb.py
@@ -1,5 +1,4 @@
-from typing import Any, TYPE_CHECKING
-
+from typing import Any
 
 from ...atomattrs import AtomAttrs
 from ...atomcollection import AtomCollection
@@ -11,6 +10,7 @@ PDB_FORMAT = (
     "{x:8.3f}{y:8.3f}{z:8.3f}{occupancy:6.2f}{bfactor:6.2f}          "
     "{element:>2s}"
 )
+
 
 def to_pdb(atom_or_collection: AtomAttrs | AtomCollection) -> str:
     if isinstance(atom_or_collection, AtomCollection):
@@ -58,5 +58,3 @@ def _format_residue_name(name: str) -> str:
 
 def _format_residue_index(index: int) -> str:
     return f"{index:4d}" if index < 10000 else f"{index:04x}"
-
-

ptools/__init__.py
@@ -14,7 +14,6 @@ from . import (
     io,
     pairlist,
     rigidbody,
-    spatial,
     superpose,
     tables,
 )
@@ -30,7 +29,6 @@ __all__ = [
     "io",
     "pairlist",
     "rigidbody",
-    "spatial",
     "superpose",
     "tables",
     "AttractRigidBody",

ptools/_typing.py
@@ -14,14 +14,15 @@ ArrayLike = Union[Sequence[Numeric], np.ndarray]
 
 class HasCoordinates(Protocol):
     @property
-    def coordinates(self) -> ArrayLike:
+    def coordinates(self) -> np.ndarray:
         ...
+
 
 class Topology(Protocol):
     @property
-    def coordinates(self) -> ArrayLike:
+    def coordinates(self) -> np.ndarray:
         ...
 
     @property
-    def masses(self) -> ArrayLike:
+    def masses(self) -> np.ndarray:
         ...

ptools/array3d.py
@@ -86,9 +86,7 @@ def is_array3d(obj: Any) -> bool:
         return False
     if ndim == 1 and shape[0] != 3:
         return False
-    elif ndim == 2 and shape[1] != 3:
+    if ndim == 2 and shape[1] != 3:
         return False
 
     return True
-
-

ptools/atomcollection.py
@@ -45,7 +45,6 @@ class Atom(AtomAttrs):
         kwargs = {k: getattr(atom, k) for k in attrs}
         super().__init__(**kwargs)
 
-
     @property
     def coords(self) -> array3d:
         """Gets atom cartesian coordinates."""
@@ -121,7 +120,9 @@ class AtomCollection(UserList):
         if not isinstance(other, AtomCollection):
             other = AtomCollection(other)
         output = super().__add__(other.copy())
-        output.coordinates = np.concatenate((self.coordinates, other.coordinates), axis=0)
+        lhs = np.asarray(self.coordinates)
+        rhs = np.asarray(other.coordinates)
+        output.coordinates = np.concatenate((lhs, rhs), axis=0)
         return output
 
     def __iadd__(self, other: Iterable[AtomAttrs]) -> AtomCollection:

ptools/attract.py
@@ -11,6 +11,7 @@ from .linalg import transformation_matrix
 from .rigidbody import AttractRigidBody
 from . import measure, transform
 
+
 def _function(x: np.ndarray, ff: AttractForceField1) -> float:
     """Function to minimize.
 

ptools/cli/attract_cmd.py
@@ -124,7 +124,7 @@ PTools revision {ptools.__version__}
     if args.startconfig:
         print("Minimize from starting configuration")
         # Use transnb, rotnb = 0, 0 to indicate this
-        translations = {0: ligand.centroid()}
+        translations = {0: ptools.measure.centroid(ligand)}
         rotations = {0: (0, 0, 0)}
     else:
         ptools.io.assert_file_exists(

commit 254d9c553dcaa9fe98480d2b34357f17155bb4d5

ptools.io.pdb: fixes bug introduced in 40d2eb0

ptools/io/pdb.py
@@ -27,7 +27,7 @@ class PDBLine(str):
     """
 
     def __init__(self, *args) -> None:
-        super().__init__(*args)
+        super().__init__()
         self.header = self[0:6].strip()
 
     def is_atom(self) -> bool: