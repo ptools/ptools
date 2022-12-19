324896620b130aed23d7f1cd183e6c3016d59900
Hubert <hubert.santuz@gmail.com>
2022-12-19 16:26:30 +0100

    add chain_intersect function

--- ptools/heligeom.py
+++ ptools/heligeom.py
@@ -59,6 +59,23 @@ def distAxis(mono,hp):
 
     return dmin, dmax
 
+def chain_intersect(rb1, rb2, delta_resid=0):
+    """
+    Return new RigidBodies containing only the common residues of the input RigidBodies.
+    - Assumes RigidBodies contain a single chain. If multiple chains, split before calling
+    and assemble results chain-by-chain.
+    - Assumes CA atoms are present for all residues
+    - Block alignment of resids is allowed through specifying a resid offset
+    - Differences in the numbers of atoms in corresponding residues of the two input RigidBodies are preserved
+    """
+    resids1 = set([ a.resid for a in rb1.select_atom_type("CA")])
+    resids2 = set([ a.resid - delta_resid for a in rb2.select_atom_type("CA")])
+    resids = resids1.intersection(resids2)
+    rb1new = RigidBody(atoms=[ a for a in rb1 if a.resid in resids])
+    rb2new = RigidBody(atoms=[ a for a in rb2 if a.resid - delta_resid in resids])
+    return rb1new, rb2new
+
+
 def heli_analyze(mono1: RigidBody, mono2: RigidBody) -> Screw:
     """Returns the screw transformation from mono1 to mono2."""
     return mat_trans_2_screw(fit_matrix(mono1, mono2))
