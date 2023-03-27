ptools/ptools

commit fa1aed2ae9e7b18a1ab37eeb751bccf246681467

minor refactoring: uses pathlib.Path

ptools/io/__init__.py
@@ -1,6 +1,5 @@
 """linalg - Defines functions to read/write files."""
 
-import os
 import pathlib
 from typing import Optional
 from .._typing import FilePath
@@ -64,17 +63,11 @@ def backup_if_exists(source: FilePath):
 
     Files will be renamed file.1, file.2, etc.
     """
-
-    def new_backup_name():
-        return os.path.join(dirname, f"{basename}.{idx}")
-
-    if os.path.exists(source):
-        dirname = os.path.dirname(source)
-        basename = os.path.basename(source)
+    source = pathlib.Path(source)
+    if source.exists():
         idx = 1
-        target = new_backup_name()
-
-        while os.path.exists(target):
+        target = source.with_suffix(f".{idx}")
+        while target.exists():
             idx += 1
-            target = new_backup_name()
-        os.rename(source, target)
+            target = source.with_suffix(f".{idx}")
+        source.rename(target)

tests/attract/__init__.py
@@ -1,15 +1,16 @@
 import os
+from pathlib import Path
 
 
-TEST_ATTRACT_PARAMS = os.path.join(os.path.dirname(__file__), "data", "attract.inp")
-TEST_ATTRACT_PARAMS_WITH_LIGAND = os.path.join(
-    os.path.dirname(__file__), "data", "attract_ligand.inp"
-)
+ATTRACT_DATA_TEST_DIR = Path(__file__).parent / "data"
 
-TEST_RECEPTOR_RED = os.path.join(os.path.dirname(__file__), "data", "receptor.red")
-TEST_LIGAND_RED = os.path.join(os.path.dirname(__file__), "data", "ligand.red")
+TEST_ATTRACT_PARAMS = ATTRACT_DATA_TEST_DIR / "attract.inp"
+TEST_ATTRACT_PARAMS_WITH_LIGAND = ATTRACT_DATA_TEST_DIR / "attract_ligand.inp"
 
-TEST_AMINON = os.path.join(os.path.dirname(__file__), "data", "aminon.par")
+TEST_RECEPTOR_RED = ATTRACT_DATA_TEST_DIR / "receptor.red"
+TEST_LIGAND_RED = ATTRACT_DATA_TEST_DIR / "ligand.red"
+
+TEST_AMINON = ATTRACT_DATA_TEST_DIR / "aminon.par"
 
 
 # Example of a reduced PDB (RED) file content.

tests/attract/test_attract.py
@@ -19,11 +19,11 @@ class CaptureStderrTest(unittest.TestCase):
 
 class TestAttract(CaptureStderrTest):
     def test_receptor_not_found(self):
-        args = ["attract", "-r", "foo", "-l", TEST_LIGAND_RED]
+        args = ["attract", "-r", "foo", "-l", str(TEST_LIGAND_RED)]
         with self.assertRaises(FileNotFoundError):
             ptools_cli(args)
 
     def test_ligand_not_found(self):
-        args = ["attract", "-r", TEST_RECEPTOR_RED, "-l", "bar"]
+        args = ["attract", "-r", str(TEST_RECEPTOR_RED), "-l", "bar"]
         with self.assertRaises(FileNotFoundError):
             ptools_cli(args)

tests/heligeom/test_heligeom.py
@@ -1,5 +1,4 @@
 import math
-import os
 from pathlib import Path
 import unittest
 
@@ -63,9 +62,7 @@ class TestHeligeomSimple(unittest.TestCase):
         """Non-regression test for heligeom.heli_construct."""
         hp = heli_analyze(self.mono1, self.mono2)
         result = heli_construct(self.mono1, hp, N=15)
-        reference_file = os.path.join(
-            TEST_DATA_DIR, "test_heli_construct_simple_result.npy"
-        )
+        reference_file = TEST_DATA_DIR / "test_heli_construct_simple_result.npy"
         reference = np.load(reference_file)
         assert_array_almost_equal(result.coordinates, reference)
 

tests/test_forcefield.py
@@ -1,6 +1,7 @@
 """Test for ptools.forcefield module."""
 
 import os
+from pathlib import Path
 import unittest
 
 import numpy as np
@@ -27,9 +28,7 @@ from .testing.moreassert import assert_array_almost_equal
 class TestAttractForceField1DummyRigid(unittest.TestCase):
     """Tests for AttractForceField1 that do not required an actual AttractRigidBody."""
 
-    path_ff_parameters = os.path.join(
-        os.path.dirname(__file__), "data", "ff_parameters.np"
-    )
+    path_ff_parameters = Path(__file__).parent / "data" / "ff_parameters.np"
 
     def setUp(self):
         self.receptor = AttractRigidBody.from_red(TEST_RECEPTOR_RED)
@@ -42,13 +41,13 @@ class TestAttractForceField1DummyRigid(unittest.TestCase):
 
     def _save_forcefield_parameters(self):
         """Save forcefield parameters for later use in regression tests."""
-        with open(self.path_ff_parameters, "wb") as f:
+        with self.path_ff_parameters.open("wb") as f:
             np.save(f, self.ff._attractive_parameters)
             np.save(f, self.ff._repulsive_parameters)
 
     def _load_forcefield_parameters(self) -> tuple[np.ndarray, np.ndarray]:
         """Load forcefield parameters for regression tests."""
-        with open(self.path_ff_parameters, "rb") as f:
+        with self.path_ff_parameters.open("rb") as f:
             attractive = np.load(f)
             repulsive = np.load(f)
         return (attractive, repulsive)

commit 312b54225a4ad383a67cb33dbe8593fe3f1e9b6e

fixes bug introduced in fa1aed2

ptools/io/__init__.py
@@ -66,8 +66,8 @@ def backup_if_exists(source: FilePath):
     source = pathlib.Path(source)
     if source.exists():
         idx = 1
-        target = source.with_suffix(f".{idx}")
+        target = source.with_suffix(source.suffix + f".{idx}")
         while target.exists():
             idx += 1
-            target = source.with_suffix(f".{idx}")
+            target = source.with_suffix(source.suffix + f".{idx}")
         source.rename(target)