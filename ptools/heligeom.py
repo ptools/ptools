ptools/ptools

commit fcee6a954a10b0c6b34bf8517b6addaf9946ae63

heligeom.chain_intersect: fixes outrageous execution time

ptools/heligeom.py
@@ -31,12 +31,12 @@ def chain_intersect(
     Returns:
         A tuple wit the 2 new RigidBodies.
     """
-    resids1 = set(a.residue_index for a in rb1.select_atom_type("CA"))
-    resids2 = set(a.residue_index - delta_resid for a in rb2.select_atom_type("CA"))
-    resids = resids1.intersection(resids2)
+    resids1 = set(rb1.select_atom_type('CA').atom_properties.get("residue_indices").values)
+    resids2 = set(rb2.select_atom_type('CA').atom_properties.get("residue_indices").values - delta_resid)
+    resids = list(resids1.intersection(resids2))
 
-    atom_ids1 = np.where(np.isin(rb1.atom_properties.get("residue_indices").values, list(resids)))[0]
-    atom_ids2 = np.where(np.isin(rb2.atom_properties.get("residue_indices").values, list(resids)))[0]
+    atom_ids1 = np.where(np.isin(rb1.atom_properties.get("residue_indices").values, resids))[0]
+    atom_ids2 = np.where(np.isin(rb2.atom_properties.get("residue_indices").values, resids))[0]
 
     return rb1[atom_ids1], rb2[atom_ids2]
 

commit 33dc6962cdbf72214c173a5764c9a7b39fba2777

heligeom.chain_intersect: fixes bug introduced in fcee6a9

ptools/heligeom.py
@@ -36,7 +36,7 @@ def chain_intersect(
     resids = list(resids1.intersection(resids2))
 
     atom_ids1 = np.where(np.isin(rb1.atom_properties.get("residue_indices").values, resids))[0]
-    atom_ids2 = np.where(np.isin(rb2.atom_properties.get("residue_indices").values, resids))[0]
+    atom_ids2 = np.where(np.isin(rb2.atom_properties.get("residue_indices").values - delta_resid, resids))[0]
 
     return rb1[atom_ids1], rb2[atom_ids2]
 