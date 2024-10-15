ptools/ptools

commit a909724037b4f39af8e75129815859a36001c98b

select with list of residue indices

ptools/particlecollection.py
@@ -308,6 +308,14 @@ class ParticleCollection:
             indices = np.array([indices[0], indices[0] + 1])
         return self[indices]
 
+    def select_residue_indices(self: ParticleCollectionType, residues: Iterable[int]) -> ParticleCollectionType:
+        """Returns a new collection with the selected residues."""
+        indices = np.where(np.isin(self.atom_properties.get("residue_indices").values, residues))[0]
+        # Always return a ParticleCollection even with only one indice
+        if indices.size == 1:
+            indices = np.array([indices[0], indices[0] + 1])
+        return self[indices]
+
     def select_residue_range(
         self: ParticleCollectionType, start: int, end: int
     ) -> ParticleCollectionType:

ptools/namedarray.py
@@ -67,6 +67,10 @@ class NamedArray:
         """Returns the number of elements stored in the property."""
         return self.values.size
 
+    def tolist(self) -> list:
+        """Returns the values as a list."""
+        return self.values.tolist()
+
     def copy(self):
         return copy.deepcopy(self)
 

tests/test_particlecollection.py
@@ -350,6 +350,12 @@ def test_select_atom_types():
     assert sorted(sel.names.tolist()) == ["CA"] * 426 + ["CB"] * 64
 
 
+def test_select_residue_indices():
+    atoms = ptools.read_pdb(TEST_LIGAND)
+    sel = atoms.select_residue_indices([8, 18])
+    assert len(sel) == 5
+    assert sel.atom_properties.get("residue_names").tolist() == ["GLN", "GLN", "GLN", "ALA", "ALA"]
+
 def test_select_residue_range():
     atoms = ptools.read_pdb(TEST_LIGAND)
     sel = atoms.select_residue_range(10, 20)

commit ba65e8509b266036f916adcd105f968b4a2584a3

fixes linting issue introduced in a9097240

ptools/particlecollection.py
@@ -288,7 +288,7 @@ class ParticleCollection:
     # == Selection methods ==============================================================
 
     def select_atom_type(self: ParticleCollectionType, atom_type: str
-    ) -> ParticleCollectionType:
+                         ) -> ParticleCollectionType:
         """Returns a new collection with the selected atom type."""
         indices = np.where(self.atom_properties.get("names").values == atom_type)[0]
         # Always return a ParticleCollection even with only one indice
@@ -301,14 +301,14 @@ class ParticleCollection:
     ) -> ParticleCollectionType:
         """Returns a new collection with the selected atom types."""
         indices = np.where(
-            np.isin(self.atom_properties.get("names").values, atom_types) #type: ignore[arg-type]
+            np.isin(self.atom_properties.get("names").values, atom_types)  # type: ignore[arg-type]
         )[0]
         # Always return a ParticleCollection even with only one indice
         if indices.size == 1:
             indices = np.array([indices[0], indices[0] + 1])
         return self[indices]
 
-    def select_residue_indices(self: ParticleCollectionType, residues: Iterable[int]) -> ParticleCollectionType:
+    def select_residue_indices(self: ParticleCollectionType, residues: ArrayLike) -> ParticleCollectionType:
         """Returns a new collection with the selected residues."""
         indices = np.where(np.isin(self.atom_properties.get("residue_indices").values, residues))[0]
         # Always return a ParticleCollection even with only one indice
@@ -362,9 +362,9 @@ class ParticleCollection:
     def iter_residues(self) -> Iterator[ParticleCollectionType]:
         """Iterates over the residues in the collection."""
         by_residue = self.groupby(lambda atom: (atom.residue_index, atom.chain))
-        return iter(by_residue.values()) #type: ignore[arg-type]
+        return iter(by_residue.values())  # type: ignore[arg-type]
 
     def iter_chains(self) -> Iterator[ParticleCollectionType]:
         """Iterates over the chains in the collection."""
         by_chain = self.groupby(lambda atom: atom.chain)
-        return iter(by_chain.values()) #type: ignore[arg-type]
+        return iter(by_chain.values())  # type: ignore[arg-type]