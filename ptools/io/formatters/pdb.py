a2261df8854ef072cb8a2dfecda87fa3e09c1d2c
Benoist LAURENT <benoist.laurent@cnrs.fr>
2024-11-04 14:16:23 +0100

    to_pdb: drastic improvement for ParticleCollection instances

--- ptools/io/formatters/pdb.py
+++ ptools/io/formatters/pdb.py
@@ -1,4 +1,6 @@
 from typing import Any, Iterable, Protocol
+from ...particlecollection import ParticleCollection
+from ...namedarray import NamedArrayContainer
 
 PDB_FORMAT = (
     "{record:<6s}{atom_index:5s} "
@@ -51,7 +53,26 @@ class PDBConvertible(Protocol):
         ...
 
 
+class ParticleBuffer:
+    """Returns a Particle properties."""
+
+    def __init__(self, properties: NamedArrayContainer, i: int):
+        self.coordinates = properties.get("coordinates")[i]
+        self.name = properties.get("names")[i]
+        self.index = properties.get("indices")[i]
+        self.residue_name = properties.get("residue_names")[i]
+        self.residue_index = properties.get("residue_indices")[i]
+        self.chain = properties.get("chains")[i]
+        self.element = properties.get("elements")[i]
+
+
 def to_pdb(atom_or_collection: PDBConvertible | Iterable[PDBConvertible]) -> str:
+    """Converts an atom or a collection of atoms to PDB format."""
+
+    if isinstance(atom_or_collection, ParticleCollection):
+        properties = atom_or_collection.atom_properties
+        return "\n".join(format_atom(ParticleBuffer(properties, i)) for i in range(len(atom_or_collection)))
+
     if isinstance(atom_or_collection, Iterable):
         return "\n".join(format_atom(atom) for atom in atom_or_collection)
     return format_atom(atom_or_collection)
@@ -65,11 +86,11 @@ def format_atom(atom: PDBConvertible) -> str:
         "occupancy": 1.0,
         "bfactor": 0.0,
         "element": atom.element,
-        "chain": _format_chain(atom.chain),
-        "atom_name": _format_atom_name(atom.name),
-        "atom_index": _format_atom_index(atom.index),
-        "residue_name": _format_residue_name(atom.residue_name),
-        "residue_index": _format_residue_index(atom.residue_index),
+        "chain": format_chain_token(atom.chain),
+        "atom_name": format_atom_name_token(atom.name),
+        "atom_index": format_atom_index_token(atom.index),
+        "residue_name": format_residue_name_token(atom.residue_name),
+        "residue_index": format_residue_index_token(atom.residue_index),
         "x": atom.coordinates[0],
         "y": atom.coordinates[1],
         "z": atom.coordinates[2],
@@ -77,22 +98,22 @@ def format_atom(atom: PDBConvertible) -> str:
     return PDB_FORMAT.format(**fmt_args)
 
 
-def _format_chain(chain: str) -> str:
+def format_chain_token(chain: str) -> str:
     assert len(chain) < 2
     return " " if not chain else chain
 
 
-def _format_atom_name(name: str) -> str:
+def format_atom_name_token(name: str) -> str:
     return f"{name:>4s}" if len(name) > 2 else name.center(4)
 
 
-def _format_atom_index(index: int) -> str:
+def format_atom_index_token(index: int) -> str:
     return f"{index:5d}" if index < 100000 else f"{index:05x}"
 
 
-def _format_residue_name(name: str) -> str:
+def format_residue_name_token(name: str) -> str:
     return f"{name:<4s}" if len(name) > 3 else name.center(4)
 
 
-def _format_residue_index(index: int) -> str:
+def format_residue_index_token(index: int) -> str:
     return f"{index:4d}" if index < 10000 else f"{index:04x}"
