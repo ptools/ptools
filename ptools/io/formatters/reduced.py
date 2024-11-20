ptools/ptools

commit b84958d781ecf9a78aaa42f106dbe299fc4b4a94

NamedArrayContainer: remove get method to make syntax more numpy.recarray lookalike

ptools/particlecollection.py
@@ -45,9 +45,8 @@ class Particle:
 
     def __getattr__(self, name):
         if name in self._singular_to_plural:
-            return self._collection.atom_properties.get(self._singular_to_plural[name])[
-                self._index
-            ]
+            plural = self._singular_to_plural[name]
+            return self._collection.atom_properties[plural][self._index]
         raise AttributeError(f"No such attribute: {name!r}")
 
     def __setattr__(self, name, value):
@@ -194,7 +193,7 @@ class ParticleCollection:
 
     def reset_atom_indices(self, start=1):
         """Resets the atom indices to a new range."""
-        self.atom_properties.set("indices", np.arange(start, start + self.size()))
+        self.atom_properties["indices"] = np.arange(start, start + self.size())
 
     def __iter__(self):
         """Iterates over the atoms."""
@@ -218,7 +217,7 @@ class ParticleCollection:
 
         atom_properties = self.__getattribute__("atom_properties")
         if name in atom_properties:
-            return self.atom_properties.get(name).values
+            return self.atom_properties[name].values
         raise AttributeError(f"{self.__class__.__name__} has no attribute: {name!r}")
 
     def __setattr__(self, name, value):
@@ -230,10 +229,10 @@ class ParticleCollection:
             # If the collection has a parent (i.e. is a slice), we need to update
             # the parent's properties.
             if self.has_parent():
-                values = self._selection.parent.atom_properties.get(name).values
+                values = self._selection.parent.atom_properties[name].values
                 values[self._selection.indices] = value
             else:
-                self.atom_properties.set(name, value)
+                self.atom_properties[name] = value
             return
         super().__setattr__(name, value)
 
@@ -305,7 +304,7 @@ class ParticleCollection:
         if "elements" not in self.atom_properties:
             self.atom_properties.add_array("element", "elements", values)
         else:
-            self.atom_properties.get("elements").values = values
+            self.atom_properties["elements"].values = values
 
     def guess_masses(self, guess_element: bool = True):
         """Guesses the masses of the atoms."""
@@ -316,13 +315,12 @@ class ParticleCollection:
         if "masses" not in self.atom_properties:
             self.atom_properties.add_array("mass", "masses", values)
         else:
-            self.atom_properties.set("masses", values)
+            self.atom_properties["masses"] = values
 
     # == Selection methods ==============================================================
     def select(self: ParticleCollectionType, selection_str: str) -> ParticleCollectionType:
         """Returns a new collection with the selected atoms."""
         return select_atoms(selection_str, self)
-
 
     def select_by_property(self: ParticleCollectionType, property_name: str, value: Any) -> ParticleCollectionType:
         """Selects atoms based on a property."""

ptools/heligeom.py
@@ -86,7 +86,7 @@ def heli_construct(rb: RigidBody, hp: Screw, N: int, Z: bool = False) -> RigidBo
         origin[:] = np.inner(origin, t_matrix[:3, :3])
 
     chains = [string.ascii_uppercase[chain_id % 26]] * len(rb_orig)
-    rb_orig.atom_properties.set("chains", chains)
+    rb_orig.atom_properties["chains"] = chains
     chain_id += 1
 
     final = rb_orig.copy()
@@ -96,7 +96,7 @@ def heli_construct(rb: RigidBody, hp: Screw, N: int, Z: bool = False) -> RigidBo
         transform.translate(rb_orig, axis * hp.normtranslation)
 
         chains = [string.ascii_uppercase[chain_id % 26]] * len(rb_orig)
-        rb_orig.atom_properties.set("chains", chains)
+        rb_orig.atom_properties["chains"] = chains
         chain_id += 1
 
         final += rb_orig

ptools/io/formatters/pdb.py
@@ -65,23 +65,23 @@ class ParticleBuffer:
     """Returns a Particle properties."""
 
     def __init__(self, properties: NamedArrayContainer, i: int):
-        self.coordinates = properties.get("coordinates")[i]
-        self.name = properties.get("names")[i]
-        self.index = properties.get("indices")[i]
-        self.residue_name = properties.get("residue_names")[i]
-        self.residue_index = properties.get("residue_indices")[i]
-        self.chain = properties.get("chains")[i]
+        self.coordinates = properties["coordinates"][i]
+        self.name = properties["names"][i]
+        self.index = properties["indices"][i]
+        self.residue_name = properties["residue_names"][i]
+        self.residue_index = properties["residue_indices"][i]
+        self.chain = properties["chains"][i]
 
         self.element = ''
         self.bfactor = 0.0
         self.occupancy = 1.0
 
         if "elements" in properties:
-            self.element = properties.get("elements")[i]  # type: ignore
+            self.element = properties["elements"][i]  # type: ignore
         if "bfactors" in properties:
-            self.bfactor = properties.get("bfactors")[i]  # type: ignore
+            self.bfactor = properties["bfactors"][i]  # type: ignore
         if "occupancies" in properties:
-            self.occupancy = properties.get("occupancies")[i]  # type: ignore
+            self.occupancy = properties["occupancies"][i]  # type: ignore
 
 
 def to_pdb(atom_or_collection: PDBConvertible | Iterable[PDBConvertible]) -> str:

ptools/namedarray.py
@@ -159,12 +159,19 @@ class NamedArrayContainer(collections.abc.Container):
     def __iter__(self) -> Iterator[NamedArray]:
         return iter(self._properties.values())
 
-    def __getitem__(self, key: int | slice) -> NamedArrayContainer:
-        """Returns a new collection with a slice of all properties."""
+    def __getitem__(self, key: str | int | slice) -> NamedArrayContainer:
+        if isinstance(key, str):
+            return self._get_by_name(key)
         if isinstance(key, int):
             key = slice(key, key + 1)
         # mypy-ignore: ``key`` is a slice, ``prop[key]`` is NamedArray
         return NamedArrayContainer(prop[key] for prop in self._properties.values())  # type: ignore
+
+    def __setitem__(self, key: str, value: NamedArrayContainer):
+        if isinstance(key, str):
+            self._set_by_name(key, value)
+            return
+        raise KeyError(f"cannot set item with key {key!r}")
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, self.__class__):
@@ -222,18 +229,18 @@ class NamedArrayContainer(collections.abc.Container):
     def iter_arrays(self) -> Iterator[NamedArray]:
         return iter(self._properties.values())
 
-    def get(self, plural: object) -> NamedArray:
+    def _get_by_name(self, plural: object) -> NamedArray:
         """Returns the property with the given plural name."""
         if not isinstance(plural, str):
             raise ValueError("expects string to fetch property using its plural name")
         return self._properties[plural]
 
-    def set(self, plural: object, value: ArrayLike):
+    def _set_by_name(self, plural: object, value: ArrayLike):
         """Sets the property with the given plural name."""
         if not isinstance(plural, str):
-            raise ValueError("expects string to set property using its plural name")
+            raise KeyError("expects string to set property using its plural name")
         if plural not in self._properties:
-            raise ValueError(f"property {plural!r} not registered")
+            raise KeyError(f"property {plural!r} not registered")
         if not self._properties[plural].values.shape == np.shape(value):
             raise ValueError(
                 f"cannot set property {plural!r} with array of shape {np.shape(value)} "

ptools/selection/precedenceparser.py
@@ -65,8 +65,6 @@ class EvaluatorBase(ABC):
         return token
 
     def _expect(self, token: str):
-        if not self._has_next():
-            raise UnexpectedTrailingTokenError("")
         if self._next() == token:
             self._consume()
         else:

ptools/selection/selectionparser.py
@@ -124,20 +124,20 @@ class IntegerPropertySelection:
 
     def select_from_operator(self, atoms: "ParticleCollection", op: Callable, value: Numeric):
         """Selection from a single operator."""
-        indices = np.where(op(atoms.atom_properties.get(self.attr).values, value))[0]
+        indices = np.where(op(atoms.atom_properties[self.attr].values, value))[0]
         return atoms[indices]
 
     def select_from_values(self, atoms: "ParticleCollection", value: list[Numeric]):
         """Selection from a single or multiple values."""
-        indices = np.where(np.isin(atoms.atom_properties.get(self.attr).values, value))[0]
+        indices = np.where(np.isin(atoms.atom_properties[self.attr].values, value))[0]
         return atoms[indices]
 
     def select_from_range(self, atoms: "ParticleCollection", start: Numeric, end: Numeric):
         """Selection from a range of values."""
         indices = np.where(
             np.logical_and(
-                atoms.atom_properties.get(self.attr).values >= start,
-                atoms.atom_properties.get(self.attr).values <= end,
+                atoms.atom_properties[self.attr].values >= start,
+                atoms.atom_properties[self.attr].values <= end,
             )
         )[0]
         return atoms[indices]
@@ -154,7 +154,7 @@ class StringPropertySelection:
         self.attr = attr
 
     def eval(self, atoms: "ParticleCollection", values: list[str]):
-        indices = np.where(np.isin(atoms.atom_properties.get(self.attr).values, values))[0]
+        indices = np.where(np.isin(atoms.atom_properties[self.attr].values, values))[0]
         return atoms[indices]
 
 
@@ -169,7 +169,7 @@ class BoolPropertySelection:
         self.attr = attr
 
     def eval(self, atoms: "ParticleCollection", values: list[str]):
-        indices = np.where(atoms.atom_properties.get(self.attr).values == True)[0]
+        indices = np.where(atoms.atom_properties[self.attr].values == True)[0]
         return atoms[indices]
 
 
@@ -189,7 +189,7 @@ class WaterSelection:
     def eval(self, atoms: "ParticleCollection", values: list[Any]):
         assert len(values) == self.operands
         water_residues = ["HOH", "WAT", "TIP3", "TIP4", "TIP5"]
-        indices = np.where(np.isin(atoms.atom_properties.get("residue_names").values, water_residues))[0]
+        indices = np.where(np.isin(atoms.atom_properties["residue_names"].values, water_residues))[0]
         return atoms[indices]
 
 

tests/test_atom.py
@@ -1,7 +1,5 @@
 """test_atom - Tests for `ptools.atom` module."""
 
-# Unit-test specific imports
-from pytest import approx
 from .generators import generate_particlecollection
 
 from ptools.tables import atomic_masses

tests/test_namedarraycontainer.py
@@ -56,7 +56,7 @@ def test_add_array():
     assert expected.plural in container
 
     # Retrieves the property and checks its attributes.
-    prop = container.get(expected.plural)
+    prop = container[expected.plural]
     assert prop.singular == expected.singular
     assert prop.plural == expected.plural
     assert_array_almost_equal(prop.values, expected.values)
@@ -82,7 +82,7 @@ def test_register():
     container = NamedArrayContainer()
     container.register(expected)
 
-    actual = container.get(expected.plural)
+    actual = container[expected.plural]
     assert actual == expected
 
 
@@ -98,11 +98,11 @@ def test_register_copies_values():
     container = NamedArrayContainer()
     container.register(item)
 
-    assert_array_almost_equal(item.values, container.get(item.plural).values)
+    assert_array_almost_equal(item.values, container[item.plural].values)
 
     item.values = 1
-    assert_array_not_almost_equal(item.values, container.get(item.plural).values)
-    assert_array_almost_equal((1, 2, 3), container.get(item.plural).values)
+    assert_array_not_almost_equal(item.values, container[item.plural].values)
+    assert_array_almost_equal((1, 2, 3), container[item.plural].values)
 
 
 # == Test ParticleCollection.remove_array ==============================================
@@ -150,7 +150,7 @@ def test_equality():
 
     # Checks inequality of containers where one array differs from one container
     # to the other.
-    r_ones = rhs.get("ones")
+    r_ones = rhs["ones"]
     r_ones.values[:] = 13
     assert lhs != rhs
 
@@ -168,7 +168,7 @@ def test_copy():
     assert container == copy
 
     # Checks that the copy is independent from the original.
-    copy.get("ones").values[:] = 13
+    copy["ones"].values[:] = 13
     assert container != copy
 
 
@@ -181,48 +181,48 @@ def test_add():
     assert result.number_of_elements() == lhs.number_of_elements() * 2
     for prop in lhs:
         assert_array_almost_equal(
-            result.get(prop.plural).values,
-            np.concatenate((lhs.get(prop.plural).values, rhs.get(prop.plural).values)),
+            result[prop.plural].values,
+            np.concatenate((lhs[prop.plural].values, rhs[prop.plural].values)),
         )
 
 
 def test_set_values():
     container = NamedArrayContainer(generate_arrays())
-    assert container.get("ones") == [1, 1, 1, 1, 1]
+    assert container["ones"] == [1, 1, 1, 1, 1]
 
-    container.set("ones", [2, 2, 2, 2, 2])
-    assert container.get("ones") == [2, 2, 2, 2, 2]
+    container["ones"] = [2, 2, 2, 2, 2]
+    assert container["ones"] == [2, 2, 2, 2, 2]
 
 
 def test_set_values_with_wrong_dimensions_fails():
     """Checks cannot set an array with wrong dimensions."""
     container = NamedArrayContainer(generate_arrays())
-    assert container.get("ones") == [1, 1, 1, 1, 1]
+    assert container["ones"] == [1, 1, 1, 1, 1]
 
     with pytest.raises(ValueError):
         not_the_expected_array_size = container.number_of_elements() + 1
-        container.set("ones", [2] * not_the_expected_array_size)
+        container["ones"] = [2] * not_the_expected_array_size
 
 
 def test_set_values_with_wrong_property_fails():
     """Checks cannot set an array with wrong name."""
     container = NamedArrayContainer(generate_arrays())
     assert container.names() == ["ones", "twos", "threes"]
-    with pytest.raises(ValueError) as excinfo:
-        container.set("wrong_property", [2, 2, 2])
+    with pytest.raises(KeyError) as excinfo:
+        container["wrong_property"] = [2, 2, 2]
     assert "wrong_property" in str(excinfo.value)
 
 
 def test_modify_slices():
     container = NamedArrayContainer(generate_arrays())
-    assert container.get("ones") == [1, 1, 1, 1, 1]
+    assert container["ones"] == [1, 1, 1, 1, 1]
 
-    container.get("ones")[1:3] = 2
-    assert container.get("ones") == [1, 2, 2, 1, 1]
+    container["ones"][1:3] = 2
+    assert container["ones"] == [1, 2, 2, 1, 1]
 
-    subset = container.get("ones")[1:3]
+    subset = container["ones"][1:3]
     subset[:] = 3
-    assert container.get("ones") == [1, 3, 3, 1, 1]
+    assert container["ones"] == [1, 3, 3, 1, 1]
 
 
 def test_from_objects():
@@ -235,5 +235,5 @@ def test_from_objects():
     container = NamedArrayContainer.from_objects(objects)
     assert container.number_of_properties() == 2
     assert container.number_of_elements() == 3
-    assert container.get("prop1s") == [1, 1, 1]
-    assert container.get("prop2s") == [2, 2, 2]
+    assert container["prop1s"] == [1, 1, 1]
+    assert container["prop2s"] == [2, 2, 2]

commit 1ebcae5af7afb862d422f7d440579b8f86d8a8b7

formatters.reduced: fixes bug introduced by refactoring in b84958d - fixes #19

ptools/io/formatters/reduced.py
@@ -70,15 +70,15 @@ class ParticleBuffer:
     """Returns a Particle properties."""
 
     def __init__(self, properties: NamedArrayContainer, i: int):
-        self.coordinates = properties.get("coordinates")[i]
-        self.name = properties.get("names")[i]
-        self.index = properties.get("indices")[i]
-        self.residue_name = properties.get("residue_names")[i]
-        self.residue_index = properties.get("residue_indices")[i]
-        self.chain = properties.get("chains")[i]
-        self.element = properties.get("elements")[i]
-        self.typeid = properties.get("typeids")[i]
-        self.charge = properties.get("charges")[i]
+        self.coordinates = properties["coordinates"][i]
+        self.name = properties["names"][i]
+        self.index = properties["indices"][i]
+        self.residue_name = properties["residue_names"][i]
+        self.residue_index = properties["residue_indices"][i]
+        self.chain = properties["chains"][i]
+        self.element = properties["elements"][i]
+        self.typeid = properties["typeids"][i]
+        self.charge = properties["charges"][i]
 
 
 REDUCED_PDB_FORMAT = (

ptools/particlecollection.py
@@ -275,12 +275,12 @@ class ParticleCollection:
     @property
     def coordinates(self):
         """Returns the coordinates of the atoms."""
-        return self.atom_properties.get("coordinates").values
+        return self.atom_properties["coordinates"].values
 
     @coordinates.setter
     def coordinates(self, value: ArrayLike):
         """Sets the coordinates of the atoms."""
-        self.atom_properties.set("coordinates", value)
+        self.atom_properties["coordinates"] = value
 
     @property
     def particles(self):